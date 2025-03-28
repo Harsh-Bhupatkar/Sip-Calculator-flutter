efa1c78602cef20067d9efc8b53af3597beaaa7a
Harsh Bhupatkar <140888358+Harsh-Bhupatkar@users.noreply.github.com>
2025-03-28 22:18:24 +0530

    second commit

--- lib/sip.dart
+++ lib/sip.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:pie_chart/pie_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -17,6 +17,7 @@ class _SipScreenState extends State<SipScreen> {
   double final_value = 0;
   double investedAmount=0;
   double esreturn = 0;
+  Map<String,double> dataMap = {};
 
 
 
@@ -24,12 +25,15 @@ class _SipScreenState extends State<SipScreen> {
     var r = rate /12/100; //monthly interest rate
     var n = years * 12 ; // number of months
 
+    Map<String,double> dataMap = {};
+
     final_value =  principal * (pow(1+r,n) - 1) / r * (1+r) ;
     investedAmount = principal * n; //invested amount
     esreturn = final_value - investedAmount;
 
     return final_value;
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -40,70 +44,94 @@ class _SipScreenState extends State<SipScreen> {
           "Sip Calculator",
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              ask("Monthly Investment", investmentController,
-                  Icons.currency_rupee),
-              SizedBox(
-                height: 10,
-              ),
-              ask("Expected return rate(p.a)", rateController, Icons.percent),
-              SizedBox(
-                height: 10,
-              ),
-              ask("Time Period(Yr)", yearsController, Icons.timer_rounded),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                ask("Monthly Investment", investmentController,
+                    Icons.currency_rupee),
+                SizedBox(
+                  height: 10,
+                ),
+                ask("Expected return rate(p.a)", rateController, Icons.percent),
+                SizedBox(
+                  height: 10,
+                ),
+                ask("Time Period(Yr)", yearsController, Icons.timer_rounded),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
 
-                    if(investmentController.text.isEmpty || rateController.text.isEmpty || yearsController.text.isEmpty)
-                      {
-                          Fluttertoast.showToast(msg: 'Please enter all details',backgroundColor:Color.fromRGBO(232, 181, 51, 1),textColor: Colors.black );
-                      }
-                      final_value = SipRetrun(int.parse(investmentController.text), double.parse(rateController.text), double.parse(yearsController.text));
-                  });
+                      if(investmentController.text.isEmpty || rateController.text.isEmpty || yearsController.text.isEmpty)
+                        {
+                            Fluttertoast.showToast(msg: 'Please enter all details',backgroundColor:Color.fromRGBO(232, 181, 51, 1),textColor: Colors.black );
+                        }
+                        final_value = SipRetrun(int.parse(investmentController.text), double.parse(rateController.text), double.parse(yearsController.text));
+                      dataMap={
+                        "Invested Amount": investedAmount,
+                        "Estimate Returns" : esreturn
+                      };
+                    });
 
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(232, 181, 51, 1)),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Calculate',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(232, 181, 51, 1)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
+                SizedBox(
+                  height: 30,
+                ),
 
-              if(final_value>0 ) Column(
-                children: [
-                  ans("Invested Amount", investedAmount.toStringAsFixed(0)),
-                  SizedBox(height: 20,),
-                  ans("Estimated Returns", esreturn.toStringAsFixed(0)),
-                  SizedBox(height: 20,),
-                  ans("Final Value", final_value.toStringAsFixed(0))
-                ],
-              )
-               
+                if(final_value>0 ) Column(
+                  children: [
+                    ans("Invested Amount", investedAmount.toStringAsFixed(0)),
+                    SizedBox(height: 20,),
+                    ans("Estimated Returns", esreturn.toStringAsFixed(0)),
+                    SizedBox(height: 20,),
+                    ans("Final Value", final_value.toStringAsFixed(0)),
+                    SizedBox(height: 20,),
+                    PieChart(
+                        dataMap: dataMap,
+                        animationDuration: Duration(milliseconds: 500),
+                        chartType: ChartType.ring,
+                        colorList: [
+                          Color.fromRGBO(232, 181, 51, 1),
+                          Colors.blueAccent
+                        ],
+                      chartRadius: MediaQuery.of(context).size.width / 2,
+                      legendOptions: LegendOptions(
+                        legendShape: BoxShape.rectangle
+                      ),
+                      chartValuesOptions: ChartValuesOptions(
+                        showChartValueBackground: false,
+                        decimalPlaces: 0
+                      ),
+                      )
+                    ],
+                )
 
 
-            ],
+
+              ],
+            ),
           ),
         ),
       ),
@@ -132,7 +160,7 @@ class _SipScreenState extends State<SipScreen> {
             decoration: InputDecoration(
                 hintText: "10",
                 hintStyle: TextStyle(
-                    color: Colors.black,
+                    color: Colors.black38,
                     fontWeight: FontWeight.bold,
                     fontSize: 20),
                 filled: true,
